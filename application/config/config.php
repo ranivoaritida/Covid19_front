a207b655b1edcd0efd8fef99811b4d2fecafc04c
RANIVOARITIDA Sandy <ranivoaritiadasandy@gmail.com>
2021-05-09 19:22:53 +0300

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/covid-front/';
+$config['base_url'] = 'https://covid-19-front.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
